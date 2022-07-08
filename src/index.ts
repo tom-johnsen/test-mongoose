f8cbd07e7ac462c29b42358907acf66256c78ca1
Tom Johnsen <105308316+tom-johnsen@users.noreply.github.com>
2022-07-08 12:01:04 +0200

    Add CORS Fixes #1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "dotenv/config"
+import cors from "cors"
 import express from "express"
 import helmet from "helmet"
 import mongoose from "mongoose"
@@ -6,9 +7,9 @@ import comments from "./routes/v1/comments"
 import error from "./handlers/error"
 import notFound from "./handlers/404"
 
-const port = process.env["PORT"] || 3001
-const mongoConnector = process.env["MONGODB_URI"] || "mongodb://localhost"
-const mongoDB = process.env["MONGODB_DATABASE"] || "no_env"
+const port = process.env["PORT"] || 3003
+const mongoConnector = process.env["MONGODB_URI"] || "mongodb://mongo:27017/"
+const mongoDB = process.env["MONGODB_DATABASE"] || "test"
 const mongoURL = new URL(mongoDB, mongoConnector).toString()
 
 const app = express()
@@ -17,6 +18,7 @@ const v1 = express.Router()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
+app.use(cors())
 app.use("/api/v1", v1)
 
 v1.use("/comments", comments)
@@ -26,7 +28,14 @@ app.use(error)
 
 app.listen(port, () => {
     mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
-    console.log(`MongoDB connected to database \"${mongoDB}\".`)
     console.log(`Server is running on port ${port}`)
     console.log("API version 1 is available at /api/v1")
+})
+
+mongoose.connection.on("connected", () => {
+    console.log(`MongoDB connected to database \"${mongoDB}\".`)
+})
+
+app.on("connected", () => {
+    console.log(`A client has connected to the server.`)
 })
